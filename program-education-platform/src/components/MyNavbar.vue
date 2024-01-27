06a0b528baaa0677997f1373a70707cbc78f06be
Tyrfly <1071131161@qq.com>
2024-01-27 23:37:51 +0800

    Update button style in MyNavbar.vue

--- program-education-platform/src/components/MyNavbar.vue
+++ program-education-platform/src/components/MyNavbar.vue
@@ -37,7 +37,7 @@
         >
           <button
             type="button"
-            class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            class="relative rounded-full bg-blue-600 p-1 text-blue-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-blue-800"
           >
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
